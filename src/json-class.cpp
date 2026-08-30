ede423e25749acd5bedf24d1d9d5929ed5e395a9
gepuor <gerard.puor@gmail.com>
2026-08-31 00:27:11 +0200

    Basic structure updated, first steps

---
+++ src/json-class.cpp
@@ -0,0 +1,31 @@
+#include "json-class.hpp"
+
+JsonData::JsonData()
+{
+
+}
+
+template <class T>
+JsonData::JsonData(T data, JsonType type)
+{
+
+}
+
+JsonData::~JsonData()
+{
+
+}
+
+const JsonPrimitive& JsonData::getData() const
+{
+
+}
+const JsonType& JsonData::getType() const
+{
+
+}
+template <class T>
+void JsonData::setData(T newData)
+{
+
+}
--- src/json-class.hpp
+++ src/json-class.hpp
@@ -1,32 +1,92 @@
-#ifndef JSON-CLASS
-
-#define JSON-CLASS
+#ifndef JSON_CLASS_HPP
+#define JSON_CLASS_HPP
 
 #include <string>
-#include <fstream>
+#include <vector>
+#include <variant>
 
-const std::string JSON_DATA_TYPES = {"String","Number","Boolean","Null","Object","Array"};
+enum class JsonType
+{
+    String,
+    Number,
+    Boolean,
+    Null,
+    Object,
+    Array
+};
 
+using JsonPrimitive = std::variant<
+    std::string,
+    double,
+    bool,
+    std::nullptr_t
+>;
 
-template<class T>
-class Object
+class JsonData
 {
     private:
-        std::string objectDataType;
-        T content;
+        JsonPrimitive data;
+        JsonType dataType;
+
     public:
 
-}
+        JsonData();
+        template <class T>
+        JsonData(T data, JsonType type);
+        ~JsonData();
+
+        const JsonPrimitive& getData() const;
+        const JsonType& getType() const;
+        template <class T>
+        void setData(T newData);
+};
+
+
+class JsonPair
+{
+    private:
+        std::string key;
+        JsonData value;
+
+    public:
+        // TODO: constructores, getters, setters...
+};
+
+
+class JsonObject
+{
+    private:
+        std::vector<JsonPair> content;
+
+    public:
+        JsonObject();
+        ~JsonObject();
+
+        // TODO: métodos para añadir, eliminar y buscar pairs...
+};
+
+
+class JsonArray
+{
+    private:
+        std::vector<JsonData> content;
+
+    public:
+        JsonArray();
+        ~JsonArray();
+
+        // TODO: métodos para añadir, eliminar y acceder a valores...
+};
 
 
 class JsonFile
 {
     private:
-        Object * content;
-        unsigned int nObjects;
-    public:
+        JsonObject content;
 
-}
+    public:
+        // TODO: métodos para leer/escribir el archivo...
+};
 
 
 #endif
